e3001f75c6eedd9ce5557850298149c2e70f23e0
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-08-19 15:33:04 +1100

    finish

--- task3_Anagrams.cpp
+++ task3_Anagrams.cpp
@@ -2,20 +2,16 @@
 #include <map>
 #include <string>
 
-int main() {
+void anagrams(std::string &str_1, std::string& str_2, bool& is_anagram) {
 
 	std::map<int, std::string> word_1Map;
 	std::pair<int, std::string>oPair_1;
 
 	std::map<int, std::string> word_2Map;
-	std::pair<int, std::string>oPair_2;
-	
-	std::string str_1 = "zxcv";
-	std::string str_2 = "zxcv";
-	
+	std::pair<int, std::string>oPair_2;	
 
-	for (int i = 0; i < str_1.size(); ++i) {		
-		
+	for (int i = 0; i < str_1.size(); ++i) {
+
 		oPair_1 = std::make_pair(i, str_1[i]);
 		word_1Map.insert(oPair_1);
 	}
@@ -24,25 +20,43 @@ int main() {
 
 		oPair_2 = std::make_pair(i, str_2[i]);
 		word_2Map.insert(oPair_2);
-	}	
+	}
 
-	for (std::map<int, std::string>::iterator it_1 = word_1Map.begin(); it_1 != word_1Map.end(); ++it_1) {		
-		for (std::map<int, std::string>::iterator it_2 = word_2Map.begin(); it_2 != word_2Map.end(); ++it_2) {			
+	for (std::map<int, std::string>::iterator it_1 = word_1Map.begin(); it_1 != word_1Map.end(); ++it_1) {
+		for (std::map<int, std::string>::iterator it_2 = word_2Map.begin(); it_2 != word_2Map.end(); ++it_2) {
 
 			if (it_1->second == it_2->second) {
-				
+
 				word_2Map.erase(it_2);
 				break;
 			}
 		}
-		std::cout << std::endl;
 	}
 
+	if (word_2Map.empty() == 1) {
 
-	std::cout << std::endl;
-	for (std::map<int, std::string>::iterator it_2 = word_2Map.begin(); it_2 != word_2Map.end(); ++it_2) {			
-		
-		std::cout << it_2->first << it_2->second << " ";
+		is_anagram = true;
+		std::cout << is_anagram;
+
 	}
-	
+	else {
+
+		std::cout << is_anagram;
+
+	}
+}
+
+int main() {
+
+	std::string str_1;
+	std::string str_2;
+	bool is_anagram = false;
+
+	std::cout << "Enter the first word:";
+	std::cin >> str_1;
+
+	std::cout << "Enter the second word:";
+	std::cin >> str_2;
+
+	anagrams(str_1, str_2, is_anagram);
 }
